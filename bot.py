6824711873a5f6f1eadf44a221a941d4e6ab8e58
HKgame01 <74695734+HKgame01@users.noreply.github.com>
2023-07-22 19:51:11 +0530

    discord bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+import openai
+import os
+import discord
+
+TOKEN = os.environ['BOT_TOKEN']
+openai.api_key = "API"
+
+API_KEY = "API"
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():    
+    print(f"Logged in as {client.user}")
+
