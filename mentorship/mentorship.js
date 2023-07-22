58179d3ccdc313d8090f00c7886a0ae90f78569b
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-22 16:20:51 +0530

    mentor connect

---
+++ mentorship/mentorship.js
@@ -0,0 +1,57 @@
+const mentorGrid = document.getElementById('mentorGrid');
+const searchInput = document.getElementById('searchInput');
+
+async function fetchMentors() {
+    try {
+        const response = await fetch('https://randomuser.me/api/?results=12');
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error('Error fetching mentors:', error);
+        return [];
+    }
+}
+
+function displayMentors(mentors) {
+    mentorGrid.innerHTML = '';
+
+    mentors.forEach((mentor) => {
+        const mentorCard = document.createElement('div');
+        mentorCard.classList.add('mentor-card');
+        mentorCard.innerHTML = `
+            <img src="${mentor.picture.large}" alt="${mentor.name.first} ${mentor.name.last}">
+            <h3>${mentor.name.first} ${mentor.name.last}</h3>
+            <p><strong>Email:</strong> ${mentor.email}</p>
+            <p><strong>Phone:</strong> ${mentor.phone}</p>
+            <p><strong>Location:</strong> ${mentor.location.city}, ${mentor.location.country}</p>
+            <p><strong>Expertise:</strong> <span class="expertise">${mentor.login.username}</span></p>
+            <button class="connect-btn" onclick="showPopup('${mentor.name.first} ${mentor.name.last}', '${mentor.email}', '${mentor.phone}')">Connect</button>
+            <button class="schedule-call-btn" onclick="showPopup('Schedule Call with ${mentor.name.first} ${mentor.name.last}', '${mentor.email}')">Schedule Call</button>
+        `;
+        mentorGrid.appendChild(mentorCard);
+    });
+}
+
+function searchMentors() {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    if (searchTerm === '') {
+        // if the search input is empty, display all mentors
+        fetchMentors().then((mentors) => displayMentors(mentors));
+    } else {
+        // if the search input is not empty, filter mentors based on expertise
+        fetchMentors().then((mentors) => {
+            const filteredMentors = mentors.filter((mentor) =>
+                mentor.login.username.toLowerCase().includes(searchTerm)
+            );
+            displayMentors(filteredMentors);
+        });
+    }
+}
+
+function showPopup(title, email, phone = '') {
+    console.log('Popup:', title, email, phone);
+}
+
+window.addEventListener('load', () => {
+    fetchMentors().then((mentors) => displayMentors(mentors));
+});
