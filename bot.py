2c4ddba7a56a9ce96802b241e6d56e61fcd7a476
HKgame01 <74695734+HKgame01@users.noreply.github.com>
2023-07-23 17:16:44 +0530

    made more readible

--- bot.py
+++ bot.py
@@ -34,26 +34,34 @@ async def about(ctx):
 
 @bot.command()
 async def idea(ctx, *, user_idea):
-    rawidea = openai.Completion.create(engine="text-davinci-003", prompt=f'tell me a good youtube video title, description and tags according to {user_idea}', max_tokens=1000)
+    rawidea = openai.Completion.create(engine="text-davinci-003", 
+                                       prompt=f'tell me a good youtube video title, description and tags according to {user_idea}', 
+                                       max_tokens=1000)
     idea = rawidea.choices[0]["text"]
     await ctx.send(f"The best title, description and tags for your Youtube Video after reviewing your idea is\n{idea}")
 
 @bot.command()
 async def communitypost(ctx):
-    rawcomm = openai.Completion.create(engine="text-davinci-003", prompt=f'tell me a good youtube community post for the channel"', max_tokens=1000)
+    rawcomm = openai.Completion.create(engine="text-davinci-003", 
+                                       prompt=f'tell me a good youtube community post for the channel"', 
+                                       max_tokens=1000)
     comm = rawcomm.choices[0]["text"]
     await ctx.send(f"The best community for your Youtube Channel after reviewing your content is\n`{comm}`")
 
 @bot.command()
 async def script(ctx, *, video_idea):
-    scr_waw = openai.Completion.create(engine="text-davinci-003", prompt=f'write a script on "{video_idea}"', max_tokens=1000)
+    scr_waw = openai.Completion.create(engine="text-davinci-003", 
+                                       prompt=f'write a script on "{video_idea}"', 
+                                       max_tokens=1000)
     script = scr_waw.choices[0]["text"]
     await ctx.send(f"Here you go:\n{script}")
 
 @bot.command()
 async def randomidea(ctx):
     prompt = "tell me a random idea for hackathon"
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    response = openai.Completion.create(engine="text-davinci-003", 
+                                        prompt=prompt, 
+                                        max_tokens=1000)
     random_idea = response.choices[0]["text"].strip()
 
     await ctx.send(f"Here's a random Idea for the hackathon:\n{random_idea}")
